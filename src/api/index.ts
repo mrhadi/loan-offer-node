28d5d01fd3f070dd99a5a129b1812a04a2579344
Hadi Shad <hadi@mrhadi.com>
2025-02-04 13:16:13 +1100

    Handle the loan request by one api, move the user details to the loan api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,11 @@
 const router = require('express').Router()
-const { API_VER } = require('../utils/config')
 
 const loanAPI = require(`./loanAPI`)
-const userAPI = require(`./userAPI`)
 
 router.use((err, req, res, next) => {
   next()
 })
 
-router.use(`/${API_VER}/loan`, loanAPI)
-router.use(`/user`, userAPI)
+router.use(`/loan`, loanAPI)
 
 module.exports = router
--- src/api/loanAPI.ts
+++ src/api/loanAPI.ts
@@ -1,12 +1,31 @@
 const router = require('express').Router()
+const { validationResult, matchedData, body } = require('express-validator')
 
-router.get('/', () => {
-})
+router.post(
+  '/',
+  [
+    body('firstName').isString(),
+    body('lastName').isString(),
+    body('email').isEmail(),
+    body('employmentStatus').isIn(['employed', 'self-employed', 'unemployed']),
+    body('employer').if(body('employmentStatus').exists().equals('employed')).isString(),
+    body('vehiclePrice').isInt(),
+    body('deposit').isInt(),
+    body('loanPurpose').isString(),
+    body('loanTerm').isIn([1, 2, 3, 4, 5, 6, 7])
+  ],
+  (req, res) => {
+    const result = validationResult(req)
+    if (!result.isEmpty()) {
+      return res.status(422).json({ errors: result.array() })
+    }
 
-router.post('/', () => {
-})
+    const userData = matchedData(req, {
+      includeOptionals: true
+    })
 
-router.put('/', () => {
-})
+    res.status(200).send(userData)
+  }
+)
 
 module.exports = router
